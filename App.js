b1959569e4084dab35d63f718c5991af912f6903
Professor Synapse <131487882+ProfSynapse@users.noreply.github.com>
2024-09-17 12:55:23 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ObsidianNoteConverter from './components/ObsidianNoteConverter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ObsidianNoteConverter />
+    </div>
+  );
+}
+
+export default App;
