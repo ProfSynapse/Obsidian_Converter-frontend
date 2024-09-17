cff6fc014f103b75460432eb13620ab3bfbd42cc
Professor Synapse <131487882+ProfSynapse@users.noreply.github.com>
2024-09-17 18:45:13 -0400

    redoing for railway

---
+++ src/components/common/input.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Input.css';
+
+const Input = ({ 
+  type = 'text', 
+  id, 
+  value, 
+  onChange, 
+  placeholder = '', 
+  required = false, 
+  className = '' 
+}) => {
+  return (
+    <input
+      type={type}
+      id={id}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      required={required}
+      className={`custom-input ${className}`}
+    />
+  );
+};
+
+export default Input;
